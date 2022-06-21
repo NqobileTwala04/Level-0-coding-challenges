fad98bf1b4f2b0e60de1c646c52460274a109e01
NqobileTwala04 <104696390+NqobileTwala04@users.noreply.github.com>
2022-06-21 09:07:49 +0200

    made changes

--- Task_10.js
+++ Task_10.js
@@ -1,13 +1,13 @@
 function findCommonCharacters(firstString, secondString) { 
   let commonCharacters=[]; 
     let count = 0; 
-    const obj = secondString.split(""); 
+    const object = secondString.split(""); 
     for(const str of firstString){ 
-      let idx = obj.findIndex(s => s === str); 
+      let idx = object.findIndex(s => s === str); 
       if(idx >= 0){ 
-      commonCharacters.push(obj.find(s => s === str)); 
+      commonCharacters.push(object.find(s => s === str)); 
         count++; 
-        obj.splice(idx, 1); 
+        object.splice(idx, 1); 
       } 
     } 
     return commonCharacters; 
