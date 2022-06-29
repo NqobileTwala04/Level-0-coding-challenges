8a3d2d03b0f6baca1e343b6f1ff68f80e2f9a3f1
NqobileTwala04 <104696390+NqobileTwala04@users.noreply.github.com>
2022-06-30 00:34:42 +0200

    made changes

--- Task_10.js
+++ Task_10.js
@@ -1,27 +1,3 @@
-/*function findCommonCharacters(firstString, secondString) { 
-  let commonCharacters=[]; 
-    let count = 0; 
-    for (let i = 0; i < secondString.length; i++) {
-    for (let j = 0; j < firstString.length; j++) {
-      if (firstString[j].toLowerCase() === secondString[i].toLowerCase()) {
-        saveCommonLetters.push(firstString[j].toLowerCase());
-      }
-    }
-  }
-    const object = secondString.split(""); 
-    for(const str of firstString){ 
-      let idx = object.findIndex(s => s === str); 
-      if(idx >= 0){ 
-      commonCharacters.push(object.find(s => s === str)); 
-        count++; 
-        object.splice(idx, 1); 
-      } 
-    } 
-    console.log("Common letters: " + commonCharacters.join(", ")); 
-  } 
-   
-findCommonCharacters("Consequence", "frequently");*/
-
 function findCommonCharacters(firstString, secondString) {
   let commonCharacters = [];
 
