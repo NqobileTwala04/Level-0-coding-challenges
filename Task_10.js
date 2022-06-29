7178171c39f67718282c6d01d8699e832c86383f
NqobileTwala04 <104696390+NqobileTwala04@users.noreply.github.com>
2022-06-30 00:33:14 +0200

    made changes

--- Task_10.js
+++ Task_10.js
@@ -1,6 +1,13 @@
-function findCommonCharacters(firstString, secondString) { 
+/*function findCommonCharacters(firstString, secondString) { 
   let commonCharacters=[]; 
     let count = 0; 
+    for (let i = 0; i < secondString.length; i++) {
+    for (let j = 0; j < firstString.length; j++) {
+      if (firstString[j].toLowerCase() === secondString[i].toLowerCase()) {
+        saveCommonLetters.push(firstString[j].toLowerCase());
+      }
+    }
+  }
     const object = secondString.split(""); 
     for(const str of firstString){ 
       let idx = object.findIndex(s => s === str); 
@@ -13,4 +20,26 @@ function findCommonCharacters(firstString, secondString) {
     console.log("Common letters: " + commonCharacters.join(", ")); 
   } 
    
-findCommonCharacters("consequence", "frequently"); 
+findCommonCharacters("Consequence", "frequently");*/
+
+function findCommonCharacters(firstString, secondString) {
+  let commonCharacters = [];
+
+  for (let i = 0; i < secondString.length; i++) {
+    for (let j = 0; j < firstString.length; j++) {
+      if (firstString[j].toLowerCase() === secondString[i].toLowerCase()) {
+        commonCharacters.push(firstString[j].toLowerCase());
+      }
+    }
+  }
+  commonCharacters = [...new Set(commonCharacters)];
+
+  let commonLetters = "Common letters: ";
+  for (let i = 0; i < commonCharacters.length; i++) {
+    commonLetters += commonCharacters[i];
+    if (i < commonCharacters.length - 1) commonLetters += ", ";
+  }
+
+ return commonLetters;
+}
+console.log(findCommonCharacters("house", "computers"))
